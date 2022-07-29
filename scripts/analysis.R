7a44069cc8d0a3db851c1e01a11abcf8d859b9ea
finnoh <finn.hoener@student.uni-tuebingen.de>
2022-07-29 11:23:04 +0200

    Added changes (diamonds example)

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,1 +1,14 @@
 print("Runs analysis")
+
+library(tidyverse)
+library(ggplot2)
+plot_diamonds <-
+  diamonds %>% ggplot(aes(x = carat, y = price, color = color)) +
+  geom_point(alpha = 0.3) +
+  theme_bw() +
+  labs(
+    x = "Carat",
+    y = "Price",
+    color = "Diamond Color",
+    title = "The Diamond Market"
+  )
